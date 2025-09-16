1fb8f96b95303c683ca6955b3151a519e1d817da
Sahidulislam05 <sahidulislamcst@gmail.com>
2025-09-16 20:43:23 +0600

    Added react app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import "./App.css";
 import Batsman from "./batsman";
+import "./App.css";
+import { Suspense } from "react";
+import Friend from "./friend";
+
+const fetchUsers = fetch("https://jsonplaceholder.typicode.com/users").then(
+  (res) => res.json()
+);
 
 function App() {
   return (
     <>
       <h1>React</h1>
-      <Batsman></Batsman>
+      <Suspense fallback={<h3> Loading...</h3>}>
+        <Friend fetchUsers= {fetchUsers}></Friend>
+      </Suspense>
+      {/* <Batsman></Batsman> */}
     </>
   );
 }
---
+++ src/friend.jsx
@@ -0,0 +1,7 @@
+export default function Friend(fetchUsers) {
+  return (
+    <div>
+      <h4>Name: </h4>
+    </div>
+  );
+}
