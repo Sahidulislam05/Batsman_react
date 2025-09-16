33a94930d6d48813ffba45b73b487a0ab92cf061
Sahidulislam05 <sahidulislamcst@gmail.com>
2025-09-16 19:07:40 +0600

    first react app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Batsman from "./batsman";
+
+function App() {
+  return (
+    <>
+      <h1>React</h1>
+      <Batsman></Batsman>
+    </>
+  );
+}
+
+export default App;
